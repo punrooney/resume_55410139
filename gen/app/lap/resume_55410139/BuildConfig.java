ab89b658637c679b76725d646aca1c30cb59bcf7
punrooney <pun_blader@hotmail.com>
2014-09-02 18:33:01 -0700

    Resume

---
+++ gen/app/lap/resume_55410139/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package app.lap.resume_55410139;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/app/lap/resume_55410139/ImageActivity.java
@@ -0,0 +1,14 @@
+package app.lap.resume_55410139;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImageActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.image55410139);
+	}
+
+}
